9d09033885646abe86ba7ac34025f148ec8e2f8a
fatmaCevik <cevikfatma93@gmail.com>
2023-10-06 17:43:08 +0300

    Crashing The Car

--- Simple-Driving/Assets/Scripts/Car.cs
+++ Simple-Driving/Assets/Scripts/Car.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class Car : MonoBehaviour
 {
@@ -13,7 +15,14 @@ public class Car : MonoBehaviour
         transform.Rotate(0f, steerValue * turnSpeed * Time.deltaTime, 0f);
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
-    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Obstacle"))
+            SceneManager.LoadScene(0);
+           //SceneManager.LoadScene("Scene_MainMenu");
+    }
+
     public void Steer(int value)
     {
         steerValue = value;
